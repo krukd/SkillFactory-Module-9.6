f65f73de765adbcef313ff3d637638274cf59ae2
krukd <krukd87@gmail.com>
2023-05-20 16:29:29 +0200

    Task1

--- SkillFactory-Module-9.6/Program.cs
+++ SkillFactory-Module-9.6/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace SkillFactory_Module_9._6
 {
+
+    class MyException : Exception
+    {
+        public MyException(string message) : base(message)
+        {
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            Exception[] exceptions = new Exception[]
+             {
+                new ArgumentException(),
+                new DivideByZeroException(),
+                new IndexOutOfRangeException(),
+                new FormatException(),
+                new IOException(),
+                new MyException("Моё исключение")
+             };
+
+            foreach (Exception exeption in exceptions)
+            {
+                try
+                {
+                    throw exeption;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
